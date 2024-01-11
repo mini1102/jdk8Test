6b7ba3e1d9c635e71b2c23bc7ca4425d4d5a4c54
邓粒 <dengli@zsyjr.com>
2024-01-11 18:22:20 +0800

    增加测试场景

--- src/main/java/com/mini/test/CollectionList3Test.java
+++ src/main/java/com/mini/test/CollectionList3Test.java
@@ -2,7 +2,6 @@ package com.mini.test;
 
 import com.alibaba.fastjson.JSON;
 import com.mini.dto.LoginReq;
-import com.mini.dto.User;
 import com.mini.dto.UserInfo;
 
 import java.util.ArrayList;
@@ -14,12 +13,11 @@ public class CollectionList3Test {
         List<UserInfo> userInfoList = buildUserInfoList();
         List<LoginReq> reqList = buildLoginReqList();
 
-         // 新增
+        // 新增
         List<LoginReq> added = new ArrayList<>(reqList);
         added.removeIf(u -> userInfoList.stream().anyMatch(p -> u.getId().equals(p.getId())));
         System.out.println(JSON.toJSONString(added));
         //System.out.println(JSON.toJSONString(reqList));
-
 
 
         // 修改
@@ -42,6 +40,15 @@ public class CollectionList3Test {
         List<UserInfo> list = new ArrayList<>();
         list.add(new UserInfo(1L, "111111", 1));
         list.add(new UserInfo(2L, "222222", 2));
+        /*list.add(new UserInfo(1L, "111111", 1));
+        list.add(new UserInfo(2L, "222222", 2));
+        list.add(new UserInfo(3L, "222222", 3));
+        list.add(new UserInfo(4L, "222222", 4));*/
+        /*list.add(new UserInfo(2L, "222222", 2));*/
+        /*list.add(new UserInfo(1L, "111111", 1));
+        list.add(new UserInfo(2L, "222222", 2));
+        list.add(new UserInfo(3L, "222222", 3));
+        list.add(new UserInfo(4L, "222222", 4));*/
         return list;
     }
 
@@ -50,6 +57,10 @@ public class CollectionList3Test {
         list.add(new LoginReq(2L, "二二二二"));
         list.add(new LoginReq(3L, "三三三三"));
         list.add(new LoginReq(4L, "四四四四"));
+        /*list.add(new LoginReq(5L, "五五五五"));
+        list.add(new LoginReq(6L, "六六六六"));*/
+        /*list.add(new LoginReq(2L, "二二二二"));*/
+
         return list;
     }
 
